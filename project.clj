336152afcf4a55337a820ac046cd287dbfe0e19c
Luis <luis@decomputed.com>
2016-10-03 12:49:17 +0200

    Formatting

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "https://github.com/decomputed/forget-about-it"
   :license {:name "GPL3"
             :url "https://www.gnu.org/licenses/gpl-3.0-standalone.html"}
+
   :dependencies [[org.clojure/clojure "1.8.0"]]
+
   :main ^:skip-aot forget-about-it.core
+
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+
+  :profiles {
+             :uberjar {:aot :all}})
