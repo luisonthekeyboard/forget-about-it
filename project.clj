efea8a370ebb6638b56f034db2dec670bf714d0e
Luis <luis@decomputed.com>
2016-10-03 21:55:56 +0200

    Deletes tweets from the user timeline, via the REST API.

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "GPL3"
             :url "https://www.gnu.org/licenses/gpl-3.0-standalone.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [
+                 [org.clojure/clojure "1.7.0"]
+                 [twitter-api "0.7.8"]]
 
   :main ^:skip-aot forget-about-it.core
 
--- src/forget_about_it/core.clj
+++ src/forget_about_it/core.clj
@@ -1,7 +1,66 @@
 (ns forget-about-it.core
-  (:gen-class))
+  (:gen-class)
+  (:use
+   [twitter.oauth]
+   [twitter.callbacks]
+   [twitter.callbacks.handlers]
+   [twitter.api.restful]
+   [clojure.pprint])
+  (:import
+   (twitter.callbacks.protocols SyncSingleCallback)))
+
+(def my-creds (make-oauth-creds ""
+                                ""
+                                ""
+                                ""))
+
+(defn maybe-delete-statuses
+  [original-statuses]
+
+  (loop [statuses original-statuses]
+
+    (when (first statuses)
+
+      (let [status      (first statuses)
+            response    (statuses-destroy-id
+                         :oauth-creds my-creds
+                         :params {:id (:id status)
+                                  :trim_user true})
+            http-status (:status response)]
+
+        (if-not (= 200 (:code http-status))
+          (do
+            (pprint http-status)
+            (throw (RuntimeException. http-status)))
+          (recur (rest statuses)))))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!" args))
+  (do
+
+    (let [first-arg (read-string (first args))
+            tweets-to-drop (if (number? first-arg) first-arg 1)]
+
+      (loop [statuses (statuses-user-timeline
+                      :oauth-creds my-creds
+                      :params {:count 200
+                               :trim_user true
+                               :exclude_replies false
+                               :contibutor_details false
+                               :include_rts true})]
+
+        (when (and (= 200 (:code (:status statuses)))
+                   (not= tweets-to-drop (count (:body statuses))))
+
+          (do
+            (maybe-delete-statuses (drop tweets-to-drop (:body statuses)))
+            (pprint (str "Deleted " (count (:body statuses)) " tweets"))
+            (recur (statuses-user-timeline
+                      :oauth-creds my-creds
+                      :params {:count 200
+                               :trim_user 200
+                               :exclude_replies false
+                               :contibutor_details false
+                               :include_rts true}))))))
+    (pprint "done")
+    (shutdown-agents)))
